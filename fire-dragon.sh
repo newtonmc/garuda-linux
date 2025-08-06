f712894f78caa18eef354b9373d32647ccb33f24
Newton Cardoso <nmcard@gmail.com>
2025-08-06 14:51:43 -0300

    fix CONF string

--- fire-dragon.sh
+++ fire-dragon.sh
@@ -7,7 +7,7 @@ mkdir -p "$HOME/.firedragon"
 
 touch "$FIREDRAGONCFG"
 
-CONF='lockPref("identity.sync.tokenserver.uri", "<https://token.services.mozilla.com/1.0/sync/1.5>");'
+CONF='lockPref("identity.sync.tokenserver.uri", "https://token.services.mozilla.com/1.0/sync/1.5")'
 
 # Check if config already exists
 if ! grep -Fxq "$CONF" "$FIREDRAGONCFG"; then
