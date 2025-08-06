e56a42fa3d462a512f59a95a62b39f8675cf11b1
Newton Cardoso <nmcard@gmail.com>
2025-08-06 11:53:47 -0300

    Create fire-dragon.sh

---
+++ fire-dragon.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Enable Mozilla Sync
+
+FIREDRAGONCFG="$HOME/.firedragon/firedragon.overrides.cfg"
+
+mkdir -p "$HOME/.firedragon"
+
+touch "$FIREDRAGONCFG"
+
+CONF='lockPref("identity.sync.tokenserver.uri", "<https://token.services.mozilla.com/1.0/sync/1.5>");'
+
+# Check if config already exists
+if ! grep -Fxq "$CONF" "$FIREDRAGONCFG"; then
+    echo "$CONF" >> "$FIREDRAGONCFG"    
+    echo "Config done."
+else
+    echo "Config already exists."
+fi
